5367946ef142ff3b14a784d17292dd6e15b4b75f
Adityasinghvats <131326798+Adityasinghvats@users.noreply.github.com>
2024-05-02 10:45:09 +0530

    New Programmes

---
+++ basic/printfractional.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float num = 10.27;
+    int num1 = (int)num;
+    cout<<(num-num1);
+}
---
+++ basic/typecasting.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float num = 10.14;
+    int num2 = (int)num;
+    cout<<num2<<endl;
+    cout<<num+100<<endl;
+}
