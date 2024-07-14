5fabf6b6c02d74b73c21b8ae743bb9e8494a5090
Adityasinghvats <131326798+Adityasinghvats@users.noreply.github.com>
2024-07-14 22:51:53 +0530

    New header

--- 08_Vectors/03_reverseArrayUsingTwoPointers.cpp
+++ 08_Vectors/03_reverseArrayUsingTwoPointers.cpp
@@ -1,7 +1,4 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
-#include "../../../../../../MinGW/lib/gcc/mingw32/6.3.0/include/c++/bits/algorithmfwd.h"
+#include<bits/stdc++.h>
 using namespace std;
 int main(){
     vector<int> v;
