860ebd4f24f79a31fad3a48a084a0f137dfa403d
Adityasinghvats <131326798+Adityasinghvats@users.noreply.github.com>
2024-09-14 18:45:32 +0530

    OOPs starting

--- 10OOPS/class.cpp
+++ 10OOPS/class.cpp
@@ -9,19 +9,24 @@ class Player{
     void print(string name){
         cout<<name;
     }
-    // Constructor
-    Player(int id,int score,string name){
-        this->id = id;
-        this->name = name;
-        this->score = score;
-    }
+
+    // Secondary Constructor
+    // Player(int id,int score,string name){
+    //     this->id = id;
+    //     this->name = name;
+    //     this->score = score;
+    // }
 };
 int main(){
+    // Amit is an object of class player
     Player Amit;
     Amit.id = 10;
     Amit.name = "Aman";
+    cout<<"Enter the score for amit: ";
+    cin>>Amit.score;
+    cout<<Amit.score<<endl;
     // cout<<Amit.name;
     Amit.print("Aditya");
 
-    Player Aditya(1,23,"Aditya");    
+    // Player Aditya(1,23,"Aditya");    
 }
