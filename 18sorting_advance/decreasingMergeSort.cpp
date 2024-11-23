a9298d5878ab889c02b28631557fe9e40a980e73
Adityasinghvats <131326798+Adityasinghvats@users.noreply.github.com>
2024-11-23 15:12:04 +0530

    reverse merge sort

--- 18sorting_advance/decreasingMergeSort.cpp
+++ 18sorting_advance/decreasingMergeSort.cpp
@@ -1,11 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
-// TC = O(nlogn)
-// stable
 void merge(vector<int> &a, vector<int> &b, vector<int> &res){
     int i=0, j=0, k=0;
     while(i<a.size() && j<b.size()){
-        if(a[i]<=b[j]) res[k++] = a[i++];
+        if(a[i]>=b[j]) res[k++] = a[i++];
         else res[k++] = b[j++];
     }
     if(i>=a.size()){
@@ -19,32 +17,26 @@ void merge(vector<int> &a, vector<int> &b, vector<int> &res){
         }
     }
 }
-
-void mergeSort(vector<int> &v){
+void decMergesort(vector<int> &v){
     int n = v.size();
-    //base case
     if(n==1) return;
-    int n1 = n/2, n2 = n-(n/2);
-    vector<int> a(n1), b(n2);
-    //copying in new arrays
-    for(int i=0;i<n1;i++) a[i] = v[i];
-    for(int i=0;i<n2;i++) b[i] = v[n1+i];
-    //recursion
-    mergeSort(a);
-    mergeSort(b);
-    //merge
+    int s1 = n/2, s2 = n-(n/2);
+    vector<int> a(s1), b(s2);
+    for(int i=0;i<s1;i++) a[i] = v[i];
+    for(int i=0;i<s2;i++) b[i] = v[s1+i];
+    decMergesort(a);
+    decMergesort(b);
     merge(a,b,v);
     a.clear();
     b.clear();
 }
-
 int main(){
     int arr[] = {5,1,3,0,4,9,6};
     int n = sizeof(arr)/sizeof(arr[0]);
     vector<int> v(arr,arr+n);
     for(int ele:v) cout<<ele<<" ";
     cout<<endl;
-    mergeSort(v);
+    decMergesort(v);
     for(int ele:v) cout<<ele<<" ";
     return 0;
 }
