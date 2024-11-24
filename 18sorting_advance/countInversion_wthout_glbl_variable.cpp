8deffcc98178a753b24cfde1d82a00c42daa93ee
Adityasinghvats <131326798+Adityasinghvats@users.noreply.github.com>
2024-11-24 16:50:42 +0530

    count inversion

---
+++ 18sorting_advance/countInversion_wthout_glbl_variable.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+using namespace std;
+int countInversion(vector<int> &a, vector<int> &b){
+    int i=0,j=0, n1=a.size(), n2=b.size(), cnt=0;
+    while(i<n1 && j<n2){
+        if(a[i]>b[j]){
+            cnt += (n1-i);
+            j++;
+        }
+        else i++;
+    }
+    return cnt;
+}
+void merge(vector<int> &a, vector<int> &b, vector<int> &res){
+    int i=0, j=0, k=0;
+    while(i<a.size() && j<b.size()){
+        if(a[i]<=b[j]) res[k++] = a[i++];
+        else res[k++] = b[j++];
+    }
+    if(i>=a.size()){
+        while(j<b.size()){
+            res[k++] = b[j++];
+        }
+    }
+    if(j>=b.size()){
+        while(i<a.size()){
+            res[k++] = a[i++];
+        }
+    }
+}
+
+int mergeSort(vector<int> &v){
+    int count = 0;
+    int n = v.size();
+    //base case
+    if(n==1) return 0;
+    int n1 = n/2, n2 = n-(n/2);
+    vector<int> a(n1), b(n2);
+    //copying in new arrays
+    for(int i=0;i<n1;i++) a[i] = v[i];
+    for(int i=0;i<n2;i++) b[i] = v[n1+i];
+    //recursion
+    count += mergeSort(a);
+    count += mergeSort(b);
+    //count inversion
+    count += countInversion(a,b);
+    //merge
+    merge(a,b,v);
+    a.clear();
+    b.clear();
+    return count;
+}
+
+int main(){
+    int arr[] = {5,1,3,0,4,9,6};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    vector<int> v(arr,arr+n);
+    for(int ele:v) cout<<ele<<" ";
+    cout<<endl;
+    cout<<"No of inversion: "<<mergeSort(v)<<endl;
+    for(int ele:v) cout<<ele<<" ";  
+    return 0;
+}
